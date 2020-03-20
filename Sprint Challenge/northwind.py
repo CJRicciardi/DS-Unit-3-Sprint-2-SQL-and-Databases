307a6ada38fdddbcda672a24374aac45532c536d
CJ Ricciardi <ricciardistg@gmail.com>
2020-03-20 13:47:43 -0400

    SPrint Challenged Pass.

--- Sprint Challenge/northwind.py
+++ Sprint Challenge/northwind.py
@@ -23,7 +23,28 @@ result = cursor.execute(expensive_query).fetchall()
 pretty_result = pd.DataFrame(result, columns=["Item", "Price"])
 print(f"\nThe ten most expensive items are:", pretty_result)
 
-# query, execute, and result for largest category
+# query, execution and answer for average age at hire
+hire_age = """
+    SELECT AVG(HireDate - BirthDate)
+    FROM Employee;
+    """
+result3 = cursor.execute(hire_age).fetchall()
+print(f"\nThe average age of a newly hired employee at Northwind is {result3[0][0]:.2f}.")
+
+# query, execute and answer for what are the ten most expensive items and their suppliers
+expensive_supplier = """
+    SELECT p.ProductName, p.UnitPrice, s.CompanyName
+    FROM Product as p 
+    LEFT JOIN Supplier as s 
+        	ON p.SupplierID = s.ID
+    ORDER BY UnitPrice DESC
+    LIMIT 10;
+    """
+result4 = cursor.execute(expensive_supplier).fetchall()
+pretty_result4 = pd.DataFrame(result4, columns=["Item", "Price", "Supplier"])
+print(f"\nThe ten most expensive items and their supplier are:\n", pretty_result4)
+
+# query, execute, and answer for largest category
 category_count = """
     SELECT CategoryId, COUNT(CategoryId) AS "Count"
     FROM Product
